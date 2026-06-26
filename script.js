76b84c0ec2cc85564d98400b31ba6e87fb338ec6
Charumathi P <charuprabahar2002@gmail.com>
2026-06-26 21:49:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+function sendMessage(){
+
+let input=document.getElementById("user-input");
+
+let message=input.value;
+
+if(message==="") return;
+
+let chat=document.getElementById("chat-box");
+
+chat.innerHTML+=`<div class="user"><b>You:</b> ${message}</div>`;
+
+fetch("/get",{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":"application/json"
+
+},
+
+body:JSON.stringify({
+
+message:message
+
+})
+
+})
+
+.then(response=>response.json())
+
+.then(data=>{
+
+chat.innerHTML+=`<div class="bot"><b>Bot:</b> ${data.response}</div>`;
+
+chat.scrollTop=chat.scrollHeight;
+
+});
+
+input.value="";
+
+}
